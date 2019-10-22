2eda37461192318910b411a3bb52d24d90eb0503
Ali Amirnejad <webilix@gmail.com>
2019-10-22 11:06:15 +0330

    Generate signup component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, SignupComponent],
     imports: [BrowserModule],
     providers: [],
     bootstrap: [AppComponent]
